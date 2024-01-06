c3b8a7d78a87baf477a9f7d528d4bb69125381e4
Weizhi Song <songwz03@gmail.com>
2024-01-06 16:34:34 +0800

    -

--- assets/photos/prepare_photos_md.py
+++ assets/photos/prepare_photos_md.py
@@ -14,27 +14,18 @@ for each_line in open(photo_list_txt):
     photo_group = each_line_split[0]
     photo_title = each_line_split[1]
     photo_loci  = each_line_split[2]
-
     if current_group == '':
         current_group = photo_group
         photos_md_handle.write('### %s\n' % current_group)
         photos_md_handle.write('<div id="banner">\n')
-        photos_md_handle.write('\t<div class="inline-block" style="display:inline-block;"><a href="%s"><img src="%s" style="height: 120px;"></a><div><p>%s</p></div></div>\n' % (photo_loci, photo_loci, photo_title))
+        photos_md_handle.write('\t<div class="inline-block" style="display:inline-block;"><a href="%s"><img src="%s" style="height: 60px;"></a><div><p>%s</p></div></div>\n' % (photo_loci, photo_loci, photo_title))
     elif photo_group == current_group:
-        photos_md_handle.write('\t<div class="inline-block" style="display:inline-block;"><a href="%s"><img src="%s" style="height: 120px;"></a><div><p>%s</p></div></div>\n' % (photo_loci, photo_loci, photo_title))
+        photos_md_handle.write('\t<div class="inline-block" style="display:inline-block;"><a href="%s"><img src="%s" style="height: 60px;"></a><div><p>%s</p></div></div>\n' % (photo_loci, photo_loci, photo_title))
     elif photo_group != current_group:
         photos_md_handle.write('</div>\n\n\n')
         current_group = photo_group
         photos_md_handle.write('### %s\n' % current_group)
         photos_md_handle.write('<div id="banner">\n')
-        photos_md_handle.write('\t<div class="inline-block" style="display:inline-block;"><a href="%s"><img src="%s" style="height: 120px;"></a><div><p>%s</p></div></div>\n' % (photo_loci, photo_loci, photo_title))
-photos_md_handle.write('</div>\n\n\n')
-
-
-
-
-
-
-
-
+        photos_md_handle.write('\t<div class="inline-block" style="display:inline-block;"><a href="%s"><img src="%s" style="height: 60px;"></a><div><p>%s</p></div></div>\n' % (photo_loci, photo_loci, photo_title))
+photos_md_handle.write('</div>\n')
 photos_md_handle.close()
